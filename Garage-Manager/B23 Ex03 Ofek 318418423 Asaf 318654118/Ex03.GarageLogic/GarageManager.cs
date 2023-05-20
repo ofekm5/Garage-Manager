b16bd091556caf846ebaea1697761fa2d6ac379a
ofekm5 <markusofek@gmail.com>
2023-05-20 15:06:22 +0300

    added methods to manager

--- Garage-Manager/B23 Ex03 Ofek 318418423 Asaf 318654118/Ex03.GarageLogic/GarageManager.cs
+++ Garage-Manager/B23 Ex03 Ofek 318418423 Asaf 318654118/Ex03.GarageLogic/GarageManager.cs
@@ -14,9 +14,18 @@ namespace Ex03.GarageLogic
             m_AllVehicles = new Dictionary<string, Vehicle>();
         }
 
-        public void InsertVehicleToGarage(string i_LicensePlateNumber)
+        public bool IsVehicleInGarage(string i_LicensePlateNumber) //todo! the UI will use it before InsertVehicleToGarage() for validation
         {
+            m_AllVehicles[i_LicensePlateNumber].CustomerVehicleCondition = eVehicleCondition.InMaintenance;
 
+            return m_AllVehicles.ContainsKey(i_LicensePlateNumber); 
+        }
+
+        public void InsertVehicleToGarage(string i_LicensePlateNumber, Dictionary<string, string> VehicleDetails)
+        {
+            Vehicle newVehicle = new Vehicle();
+
+            m_AllVehicles.Add(i_LicensePlateNumber, newVehicle);
         }
 
         public List<string> GetListOfLicensePlates(string i_VehicleConditionFilter)
