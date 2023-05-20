056026ddd2ef96561384747f57a6fe61ac71198c
ofekm5 <markusofek@gmail.com>
2023-05-20 13:25:43 +0300

    wrote structure of GarageManager

---
+++ Garage-Manager/B23 Ex03 Ofek 318418423 Asaf 318654118/Ex03.GarageLogic/GarageManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class GarageManager
+    {
+        private Dictionary<string, Vehicle> m_AllVehicles;
+
+        public GarageManager()
+        {
+            m_AllVehicles = new Dictionary<string, Vehicle>();
+        }
+
+        public void InsertVehicleToGarage(string i_LicensePlateNumber)
+        {
+
+        }
+
+        public List<string> GetListOfLicensePlates(string i_VehicleConditionFilter)
+        {
+
+        }
+        
+        public void ChangeVehicleCondition(string i_LicensePlateNumber, string i_NewCondition)
+        {
+
+        }
+
+        public void PumpWheelsToMax(string i_LicensePlateNumber)
+        {
+
+        }
+
+        public void AddEnergy(float i_AmountToFill)
+        {
+
+        }
+
+        public string PresentVehicleData(string i_LicensePlateNumber)
+        {
+
+        }
+    }
+}
